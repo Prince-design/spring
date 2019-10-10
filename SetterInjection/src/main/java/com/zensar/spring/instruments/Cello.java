835753de7e7efb04144ba0a1217d289a44e7c15c
Prince-design <princetiwari59@gmail.com>
2019-10-09 18:06:20 -0700

    Added SetterInjection

---
+++ SetterInjection/src/main/java/com/zensar/spring/instruments/Cello.java
@@ -0,0 +1,11 @@
+package com.zensar.spring.instruments;
+
+public class Cello implements Instrument {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Cing Cing Cing");
+	}
+
+}
